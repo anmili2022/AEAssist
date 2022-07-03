7248f97148bc5b4d5bb52707b1605656d7b42d9f
KaiserBh <kaiserbh@protonmail.com>
2022-07-03 15:47:50 +1000

    fixed spacing.

--- AEAssist/DataBinding/DataBinding.cs
+++ AEAssist/DataBinding/DataBinding.cs
@@ -119,7 +119,7 @@ namespace AEAssist
             SageSettings.LucidDreamingToggle = true;
             Wildfire = true;
             SMNSettings.SMNReset();
-            PaladinSettings. PLDReset();
+            PaladinSettings.PLDReset();
         }
 
 
