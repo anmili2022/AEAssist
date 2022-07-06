ff0831b95ee0baec187b9b7e2e1be6caf97ec915
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2022-07-06 14:40:12 -0400

    Update PaladinSettingView.xaml.cs

--- AEAssist/View/PaladinSettingView.xaml.cs
+++ AEAssist/View/PaladinSettingView.xaml.cs
@@ -12,6 +12,7 @@ namespace AEAssist.View
 
             var fightorFlightTiming = new Dictionary<int, string>
             {
+                // Any means either 1 or 2 
                 { 0, "Any/任意时间" },
                 { 1, "Fast Blade/先锋剑之后" },
                 { 2, "Riot Blade/暴乱剑之后" }
